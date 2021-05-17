48cd52426b00b1ad0413ddcc5c620d4f7f96295a
carlosjunod <carlos@junod.cl>
2021-05-16 20:26:34 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+// index.js
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./src/App";
+import "./normalize.css";
+
+ReactDOM.render(<App />, document.getElementById("app"));
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h3>New Component</h3>
+    </div>
+  );
+};
+
+export default App;
